0eecb35d5fb65717f66d22571ff0f0246d134646
Grayson Nocera <gnocera@purdue.edu>
2024-10-20 15:39:46 +0000

    feat: implement initial call to DSE init

--- LLVMBasedDSE/DSE/src/Instrument.cpp
+++ LLVMBasedDSE/DSE/src/Instrument.cpp
@@ -8,7 +8,25 @@ namespace instrument {
  * Implement your instrumentation for dynamic symbolic execution engine
  */
 bool Instrument::runOnFunction(Function &F) {
-  /* Add your code here */
+  outs() << "Running " << getAnalysisName() << " on " << F.getName() << "\n";
+
+
+  // insert "call void __DSE_Init__();"
+  // before the first command
+  inst_iterator I = inst_begin(F);
+  Module *M = F.getParent();
+  std::vector<Type *> Params(0);
+  std::vector<Value *> Args(0);
+  FunctionType *FT = FunctionType::get(Type::getVoidTy(M->getContext()), Params, false);
+  FunctionCallee f = M->getOrInsertFunction(DSEInitFunctionName, FT);
+  CallInst *Call = CallInst::Create(f, Args, "", &I);
+  Call->setCallingConv(CallingConv::C);
+  Call->setTailCall(true);
+
+  for (inst_iterator I = inst_begin(F), E = inst_end(F); I != E; ++I) {
+    // do something for each instruction
+  }
+
   return true;
 }
 
